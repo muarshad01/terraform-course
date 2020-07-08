a13f28779eaa404f2e3910c5a4c1e2b272a5f771
Muhammad Umer Arshad <marshad@salesforce.com>
2020-07-08 15:44:27 -0400

    updated instnace.tf

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,10 @@
 provider "aws" {
   access_key = "ACCESS_KEY_HERE"
   secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  region     = "us-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-03f24a1c3ecf7122a"
   instance_type = "t2.micro"
 }
-
