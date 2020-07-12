252c509792be94c582b7aa2c026d9a4e8eeeb217
Muhammad Umer Arshad <marshad@salesforce.com>
2020-07-12 17:33:48 -0400

    updated demo-1

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,19 +1,13 @@
-variable "AWS_ACCESS_KEY" {
-}
-
-variable "AWS_SECRET_KEY" {
-}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+	default = "eu-west-1"
 }
 
 variable "AMIS" {
-  type = map(string)
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
-  }
+	type = map(string)
+	default = {
+		us-west-1 = "ami-03f24a1c3ecf7122a"
+	}
 }
-
