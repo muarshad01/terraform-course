c44fca3e481dcbf5680ac0fafbd1400651bf087a
Muhammad Umer Arshad <marshad@salesforce.com>
2020-07-20 16:07:24 -0400

    updated demo-3

--- demo-3/vars.tf
+++ demo-3/vars.tf
@@ -5,15 +5,13 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+	default = "us-west-1"
 }
 
 variable "AMIS" {
-  type = map(string)
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
-  }
+	type = map(string)
+	default = {
+		us-wast-1 = "ami-03f24a1c3ecf7122"
+  	}
 }
 
