d3cf4f6d75f231756f53c4ca1585c5cf311fb539
Muhammad Umer Arshad <marshad@salesforce.com>
2020-08-20 22:39:06 -0400

    updated demo-7

--- demo-7/vars.tf
+++ demo-7/vars.tf
@@ -1,12 +1,16 @@
+variable "AWS_ACCESS_KEY" {}
+  
+variable "AWS_SECRET_KEY" {}
+
 variable "AWS_REGION" {
-  default = "eu-west-1"
+	default = "us-east-2"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+	default = "mykey"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+	default = "mykey.pub"
 }
 
