c4c2b54b6f52241be569383822ceb7d30809ae26
Muhammad Umer Arshad <marshad@salesforce.com>
2020-08-20 22:50:07 -0400

    updated demo-8

--- demo-8/vars.tf
+++ demo-8/vars.tf
@@ -1,5 +1,9 @@
+variable "AWS_ACCESS_KEY" {}
+  
+variable "AWS_SECRET_KEY" {}
+
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-2"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -13,9 +17,7 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-2 = "ami-0bbe28eb2173f6167"
   }
 }
 
